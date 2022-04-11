f3f350901ca9f672f71a6e2248c035f7b1e26066
blazma <martin.blazsek@gmail.com>
2022-04-11 15:24:30 +0200

    GS results analyze

--- scripts/analyze_gridsearch.py
+++ scripts/analyze_gridsearch.py
@@ -33,12 +33,8 @@ for subdir in glob("{}/*/".format(sys.argv[1])):
 
     data.append(results_dict)
 
-gamma_power_PC = [d['gamma_power_PC'] for d in data]
-
-
-
 data_df = pandas.DataFrame(data)
-del data_df['multiplier'], data_df['np.nan']
+#del data_df['multiplier'], data_df['np.nan']
 
 pairs = [
     ("w_PC_E", "w_PC_I"),
@@ -64,29 +60,35 @@ w_PC_I_sorted = sorted(list(pandas.unique(data_df["w_PC_I"])), reverse=True)
 w_BC_E_sorted = sorted(list(pandas.unique(data_df["w_BC_E"])))
 w_BC_I_sorted = sorted(list(pandas.unique(data_df["w_BC_I"])), reverse=True)
 
-fig, axes = plt.subplots(3,3)
-cbar_ax = fig.add_axes([.95, .3, .03, .4])
-vmax = data_df["gamma_power_PC"].max() #/ data_df["ripple_power_LFP"]).max()
-for outer_x, w_PC_E in enumerate(w_PC_E_sorted):
-    axes[0][outer_x].set_title(w_PC_E, fontsize=20)
-    for outer_y, w_PC_I in enumerate(w_PC_I_sorted):
-        axes[:,0][outer_y].set_ylabel(w_PC_I, fontsize=20)
-        heatmap_matrix = numpy.zeros((3,3))
-        for inner_x, w_BC_E in enumerate(w_BC_E_sorted):
-            for inner_y, w_BC_I in enumerate(w_BC_I_sorted):
-                datapoint = data_df[
-                    (data_df["w_BC_E"] == w_BC_E) &
-                    (data_df["w_BC_I"] == w_BC_I) &
-                    (data_df["w_PC_E"] == w_PC_E) &
-                    (data_df["w_PC_I"] == w_PC_I)
-                ]
-                val = datapoint["gamma_power_PC"] #/ datapoint["ripple_power_LFP"]
-                heatmap_matrix[inner_y, inner_x] = float(val)
-        seaborn.heatmap(heatmap_matrix, ax=axes[outer_y, outer_x], vmin=0, vmax=vmax, cbar_ax=cbar_ax,
-                        xticklabels = w_BC_E_sorted,
-                        yticklabels = w_BC_I_sorted)
-        axes[outer_y, outer_x].set_xlabel("w_BC_E")
-        axes[outer_y, outer_x].set_ylabel("w_BC_I")
-        fig.text(0.5, 0.94, 'w_PC_E', ha='center', fontsize=15)
-        fig.text(0.04, 0.5, 'w_PC_I', va='center', rotation='vertical', fontsize=15)
+
+def plot_variable_as_heatmap(variable):
+    fig, axes = plt.subplots(3, 3)
+    fig.suptitle(variable)
+    cbar_ax = fig.add_axes([.95, .3, .03, .4])
+    vmax = data_df[variable].max() #/ data_df["ripple_power_LFP"]).max()
+    for outer_x, w_PC_E in enumerate(w_PC_E_sorted):
+        axes[0][outer_x].set_title(w_PC_E, fontsize=20)
+        for outer_y, w_PC_I in enumerate(w_PC_I_sorted):
+            axes[:,0][outer_y].set_ylabel(w_PC_I, fontsize=20)
+            heatmap_matrix = numpy.zeros((3,3))
+            for inner_x, w_BC_E in enumerate(w_BC_E_sorted):
+                for inner_y, w_BC_I in enumerate(w_BC_I_sorted):
+                    datapoint = data_df[
+                        (data_df["w_BC_E"] == w_BC_E) &
+                        (data_df["w_BC_I"] == w_BC_I) &
+                        (data_df["w_PC_E"] == w_PC_E) &
+                        (data_df["w_PC_I"] == w_PC_I)
+                    ]
+                    val = datapoint[variable] #/ datapoint["ripple_power_LFP"]
+                    heatmap_matrix[inner_y, inner_x] = float(val)
+            seaborn.heatmap(heatmap_matrix, ax=axes[outer_y, outer_x], vmin=0, vmax=vmax, cbar_ax=cbar_ax,
+                            xticklabels = w_BC_E_sorted,
+                            yticklabels = w_BC_I_sorted)
+            axes[outer_y, outer_x].set_xlabel("w_BC_E")
+            axes[outer_y, outer_x].set_ylabel("w_BC_I")
+            fig.text(0.5, 0.94, 'w_PC_E', ha='center', fontsize=15)
+            fig.text(0.04, 0.5, 'w_PC_I', va='center', rotation='vertical', fontsize=15)
+
+plot_variable_as_heatmap("absolute_gamma_power_PC")
+plot_variable_as_heatmap("relative_gamma_power_PC")
 plt.show()
