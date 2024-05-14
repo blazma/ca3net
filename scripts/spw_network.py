bab2a68daad077c75aa7acdcf2d1f9dc55553af8
Kovács Barna <barni02kovacs@gmail.com>
2024-05-14 12:23:32 +0200

    Next iteration spkies and replay detected, but no SPWRs

--- scripts/spw_network.py
+++ scripts/spw_network.py
@@ -125,11 +125,15 @@ dg_ampa/dt = (x_ampa - g_ampa) / rise_BC_E : 1
 dx_ampa/dt = -x_ampa/decay_BC_E : 1
 dg_gaba/dt = (x_gaba - g_gaba) / rise_BC_I : 1
 dx_gaba/dt = -x_gaba/decay_BC_I : 1
-#kerdeses
-tau_mem : second
-I_syn : amp
-R_in : ohm
-dv/dt = -v/tau_mem + (R_in*I_syn)/tau_mem : volt
+"""
+
+eqs_BC_STP = """
+I_syn_ee_synapses : ampere
+I_syn_ei_synapses : ampere
+I_syn_ie_synapses : ampere
+I_syn_ii_synapses : ampere
+dvm/dt = (-g_leak_BC*(vm-Vrest_BC) + g_leak_BC*delta_T_BC*exp((vm- theta_BC)/delta_T_BC) - w + (I_syn_ee_synapses + I_syn_ei_synapses + I_syn_ie_synapses + I_syn_ii_synapses))/Cm_BC : volt (unless refractory)
+dw/dt = (a_BC*(vm-Vrest_BC) - w) / tau_w_BC : amp
 """
 
 #Short-term plasticity with the Tsodyks, Pawelzik, Markram 1998 model
@@ -142,7 +146,7 @@ U_SE : 1
 tau_inact : second
 tau_rec : second
 z = 1 - x - y : 1 # inactive
-I_syn = A_SE*y : amp
+I_syn_{}_post = A*y : ampere (summed) #needs to replaced with the relevant type per synapse
 """
 
 synapses_action = """
@@ -152,12 +156,15 @@ x += -U_SE*x
 
 #PV+BC - PV+BC connection parameters from Kohus 2016
 
-tau_inact = 3.17 * ms
-U_SE = 0.31
-A_SE = 410 * pA
-tau_rec = 1080 * ms
-R_in = 42.12 * mohm
-tau_mem = 7.2 * ms
+U_SE_BC_I = 0.31
+A_SE_BC_I = 410 * pA
+tau_rec_BC_I = 1080 * ms
+
+#PC - PV+BC connection parameters from Ecker 2020
+
+U_SE_BC_E = 0.23
+A_SE_BC_E = 205 * pA
+tau_rec_BC_E = 410 * ms
 
 
 def run_simulation(wmx_PC_E, STDP_mode, cue, save, seed, verbose=True):
@@ -190,9 +197,9 @@ def run_simulation(wmx_PC_E, STDP_mode, cue, save, seed, verbose=True):
                       reset="vm=Vreset_PC; w+=b_PC", refractory=tref_PC, method="exponential_euler")
     PCs.vm = Vrest_PC; PCs.g_ampa = 0.0; PCs.g_ampaMF = 0.0; PCs.g_gaba = 0.0
 
-    BCs = NeuronGroup(nBCs, model=eqs_BC, threshold="vm>spike_th_BC",
+    BCs = NeuronGroup(nBCs, model=eqs_BC_STP, threshold="vm>spike_th_BC",
                       reset="vm=Vreset_BC; w+=b_BC", refractory=tref_BC, method="exponential_euler")
-    BCs.vm  = Vrest_BC; BCs.g_ampa = 0.0; BCs.g_gaba = 0.0; BCs.R_in = R_in; BCs.tau_mem = tau_mem
+    BCs.vm  = Vrest_BC
 
     MF = PoissonGroup(nPCs, rate_MF)
     C_PC_MF = Synapses(MF, PCs, on_pre="x_ampaMF+=norm_PC_MF*w_PC_MF")
@@ -215,12 +222,33 @@ def run_simulation(wmx_PC_E, STDP_mode, cue, save, seed, verbose=True):
     C_PC_I = Synapses(BCs, PCs, on_pre="x_gaba+=norm_PC_I*w_PC_I", delay=delay_PC_I)
     C_PC_I.connect(p=connection_prob_BC)
 
-    C_BC_E = Synapses(PCs, BCs, on_pre="x_ampa+=norm_BC_E*w_BC_E", delay=delay_BC_E)
+    C_BC_E = Synapses(PCs, BCs, model="""
+                    dx/dt =  z/tau_rec   : 1 (clock-driven) # recovered
+                    dy/dt = -y/tau_inact : 1 (clock-driven) # active
+                    A_SE : ampere
+                    U_SE : 1
+                    tau_inact : second
+                    tau_rec : second
+                    z = 1 - x - y : 1 # inactive
+                    I_syn_ei_synapses_post = A_SE*y : ampere (summed)
+                    """
+                    , on_pre=synapses_action, method="exponential_euler")
     C_BC_E.connect(p=connection_prob_PC)
+    C_BC_E.x = 1; C_BC_E.U_SE = U_SE_BC_E; C_BC_E.A_SE = A_SE_BC_E; C_BC_E.tau_rec = tau_rec_BC_E; C_BC_E.tau_inact = decay_BC_E
 
-    C_BC_I = Synapses(BCs, BCs, model=synapses_eqs, on_pre=synapses_action, method="exponential_euler")
+    C_BC_I = Synapses(BCs, BCs, model="""
+                    dx/dt =  z/tau_rec   : 1 (clock-driven) # recovered
+                    dy/dt = -y/tau_inact : 1 (clock-driven) # active
+                    A_SE : ampere
+                    U_SE : 1
+                    tau_inact : second
+                    tau_rec : second
+                    z = 1 - x - y : 1 # inactive
+                    I_syn_ii_synapses_post = A_SE*y : ampere (summed)
+                    """
+                    , on_pre=synapses_action, method="exponential_euler")
     C_BC_I.connect(p=connection_prob_BC)
-    C_BC_I.x = 1; C_BC_I.tau_inact = tau_inact; C_BC_I.U_SE = U_SE; C_BC_I.A_SE = A_SE; C_BC_I.tau_rec = tau_rec 
+    C_BC_I.x = 1; C_BC_I.U_SE = U_SE_BC_I; C_BC_I.A_SE = A_SE_BC_I; C_BC_I.tau_rec = tau_rec_BC_I; C_BC_I.tau_inact = decay_BC_I 
 
     SM_PC = SpikeMonitor(PCs)
     SM_BC = SpikeMonitor(BCs)
